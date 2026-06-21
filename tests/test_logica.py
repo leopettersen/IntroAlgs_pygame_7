ae59e708eac86fbf624fea3ee83575108407d292
Rafael Torres <rafaelmodestoft@gmail.com>
2026-06-21 15:41:51 -0300

    Testes de colisão

--- tests/test_logica.py
+++ tests/test_logica.py
@@ -1,6 +1,5 @@
 from src.funcoesJogo import calcular_pontos, jogador_perdeu, limitar_valor
 
-
 def test_calcular_pontos():
     """Deve somar corretamente os pontos atuais com os pontos ganhos."""
     assert calcular_pontos(10, 5) == 15
@@ -29,3 +28,22 @@ def test_limitar_valor_acima_do_maximo():
 def test_limitar_valor_dentro_do_intervalo():
     """Deve manter o valor original quando ele ja estiver no intervalo."""
     assert limitar_valor(50, 0, 100) == 50
+
+def test_verificar_colisao_entre_retangulos_sobrepostos():
+    """Deve detectar colisao entre dois retangulos."""
+    from src.funcoesMeteoro import Meteoro
+    from pygame import Rect
+    rect1 = Rect(0, 0, 60, 60)
+    meteoro = Meteoro(50, 50)
+    from src.funcoesJogo import verificar_colisao
+    assert verificar_colisao(rect1, meteoro) is True
+
+def test_verificar_colisao_entre_retangulos_distantes():
+    """Nao deve detectar colisao entre retangulos que nao se sobrepoem."""
+    
+    from src.funcoesMeteoro import Meteoro
+    from pygame import Rect
+    rect1 = Rect(0, 0, 50, 50)
+    meteoro = Meteoro(100, 100)
+    from src.funcoesJogo import verificar_colisao
+    assert verificar_colisao(rect1, meteoro) is False
